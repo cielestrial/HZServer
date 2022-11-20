04d5acb62b82576cbea1e06b0d20794e4a7e975a
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-11-20 02:17:43 -0500

    Update TestCaseService.java

--- src/main/java/com/donkeycar/HZServer/TestCaseService.java
+++ src/main/java/com/donkeycar/HZServer/TestCaseService.java
@@ -11,32 +11,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestCaseService {
 
-    private HazelcastInstance instance;
-    private ITopic<String> topic;
+  private HazelcastInstance instance;
+  private ITopic<String> topic;
 
-    public TestCaseService() {
-        Config config = new Config();
-		//config.setClusterName("Message Board");
-        config.setClassLoader(Thread.currentThread().getContextClassLoader());
-        this.instance = Hazelcast.newHazelcastInstance(config);
-    }
+  public TestCaseService() {
+    Config config = new Config();
+    // config.setClusterName("Message Board");
+    config.setClassLoader(Thread.currentThread().getContextClassLoader());
+    this.instance = Hazelcast.newHazelcastInstance(config);
+  }
 
-    public void broadcastTestCase(String message) {
-        topic = instance.getReliableTopic("run");
-        topic.publish(message);
-        System.out.println("\nPublished: " + message + "\n");
-        
-    }
+  public void broadcastTestCase(String message) {
+    topic = instance.getReliableTopic("run");
+    topic.publish(message);
+    System.out.println("\nPublished: " + message + "\n");
+  }
 
-    public String broadcastMessageResponse() {
-        String responseMessage = "";
-        for(int i = 0; i < instance.getCluster().getMembers().size(); i++) {
-            responseMessage +=
-             instance.getCluster().getMembers().toArray()[i].toString()
-             + ";";
-        }
-        StringUtils.chop(responseMessage);
-        return responseMessage;
+  public String broadcastMessageResponse() {
+    String responseMessage = "";
+    for (int i = 0; i < instance.getCluster().getMembers().size(); i++) {
+      // responseMessage +=
+      // instance.getCluster().getMembers().toArray()[i].toString()instance.getCluster().getMembers().toArray()[i].toString()
+      responseMessage += instance.getCluster().getLocalMember().getAddressMap() + ";";
     }
+    StringUtils.chop(responseMessage);
+    return responseMessage;
+  }
 
 }
